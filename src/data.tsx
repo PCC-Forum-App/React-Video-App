ad889de12fb0ee0e7cceb76eec3d53035f2a7325
linnkhant07 <lkty07@gmail.com>
2024-05-03 10:59:44 -0700

    Filter Buttons added

--- src/data.tsx
+++ src/data.tsx
@@ -1,20 +1,20 @@
 const myVideos = [
-    {
-      Title: "Flexbox Tutorial",
-      Channel: "Kenji Longid",
-      Thumbnail: "src/assets/CSS Team Thumbnail.png",
-      Type: "YouTube Video",
-      Year: "2024",
-      Link: "https://www.youtube.com/watch?v=TohziRysKKY",
-    },
-    {
-      Title: "HTML Tutorial",
-      Channel: "Leane Che",
-      Thumbnail: "src/assets/HTML Team Thumbnail.png",
-      Type: "YouTube Video",
-      Year: "2024",
-      Link: "https://youtu.be/Z1neJhEEoFA?si=24-LVC81Tyxw51z9",
-    },
-  ];
+  {
+    Title: "Flexbox Tutorial",
+    Channel: "Kenji Longid",
+    Thumbnail: "src/assets/CSS Team Thumbnail.png",
+    Type: "Frontend",
+    Year: "2024",
+    Link: "https://www.youtube.com/watch?v=TohziRysKKY",
+  },
+  {
+    Title: "HTML Tutorial",
+    Channel: "Leane Che",
+    Thumbnail: "src/assets/HTML Team Thumbnail.png",
+    Type: "Frontend",
+    Year: "2024",
+    Link: "https://youtu.be/Z1neJhEEoFA?si=24-LVC81Tyxw51z9",
+  },
+];
 
-  export default myVideos
+export default myVideos;
